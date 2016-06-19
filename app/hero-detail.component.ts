cec6df60a0c00a10d2f15973271453c734b0245e
Dmitry <zimokk1@gmail.com>
2016-06-20 00:57:02 +0200

    Added hero info directive

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core' ;
+
+import { Hero } from './hero';
+
+@Component({
+    selector: 'my-hero-detail',
+    templateUrl: 'app/hero-detail.template.html'
+})
+
+
+export class HeroDetailComponent {
+@Input()
+    hero: Hero;
+}
