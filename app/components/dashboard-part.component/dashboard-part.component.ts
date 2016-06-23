62aaf1a2ab50dc08f2b699db0262c33e073ace46
Dmitry <zimokk1@gmail.com>
2016-06-23 14:07:51 +0200

    Added dashboard-part component

---
+++ app/components/dashboard-part.component/dashboard-part.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, trigger, state, style, transition, animate} from '@angular/core' ;
+
+import { Hero } from '../../entity/hero/hero';
+
+@Component({
+    selector: 'dashboard-part',
+    templateUrl: 'app/templates/dashboard-part.template.html',
+    styleUrls: ['app/styles/dashboard.component.css'],
+})
+
+export class DashboardPartComponent {
+    @Input() hero: Hero;
+}
